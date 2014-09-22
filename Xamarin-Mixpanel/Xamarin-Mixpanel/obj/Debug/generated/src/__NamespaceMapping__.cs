f7e73f5e14620c7283edc32a282c6b6eeb6382da
Rocky Duan <dementrock@gmail.com>
2014-09-21 17:50:25 -0700

    clean up

--- Xamarin-Mixpanel/Xamarin-Mixpanel/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.mixpanel.android", Managed="Com.Mixpanel.Android")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.mixpanel.android.mpmetrics", Managed="Com.Mixpanel.Android.Mpmetrics")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.mixpanel.android.surveys", Managed="Com.Mixpanel.Android.Surveys")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.mixpanel.android.util", Managed="Com.Mixpanel.Android.Util")]
